ddd1912d1e9c57dcad117ae6e021f68845d0bf88
Hasham <hash004@gmail.com>
2016-03-23 17:48:24 +0000

    some more work

--- weatherApp/javascript/script.js
+++ weatherApp/javascript/script.js
@@ -1,7 +1,15 @@
 if ("geolocation" in navigator) {
   /* geolocation is available */
   navigator.geolocation.getCurrentPosition(function(position) {
-  $("#latlong").text("Lat: " + position.coords.latitude + " Long: " + position.coords.longitude);
+
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=3c764df6fcdd7a170df4a950b9f7a8f6", function(data){
+    //$("#latlong").text(JSON.stringify(data));
+    temperature = ((data.main.temp)-273.15).toFixed(1);
+    city = data.name;
+    $("#latlong").text("It's " + temperature + " in " + city);
+  });
+
+
 });
 } else {
   /* geolocation IS NOT available */
