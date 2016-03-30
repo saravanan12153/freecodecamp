dc5f95d58b1ab83e41e1b7ae38943fa51946e0b1
Hasham <hash004@gmail.com>
2016-03-30 17:35:53 +0100

    will work on it at home

--- weatherApp/javascript/script.js
+++ weatherApp/javascript/script.js
@@ -1,16 +1,21 @@
 if ("geolocation" in navigator) {
   /* geolocation is available */
   navigator.geolocation.getCurrentPosition(function(position) {
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=3c764df6fcdd7a170df4a950b9f7a8f6", function(data){
+      //$("#latlong").text(JSON.stringify(data));
+      temperature = ((data.main.temp)-273.15).toFixed(1);
+      city = data.name;
+      country = data.sys.country;
+      weather = data.weather[0];
+      sunrise = data.sys.sunrise;
+      sunset = data.sys.sunset;
 
-  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=3c764df6fcdd7a170df4a950b9f7a8f6", function(data){
-    //$("#latlong").text(JSON.stringify(data));
-    temperature = ((data.main.temp)-273.15).toFixed(1);
-    city = data.name;
-    $("#latlong").text("It's " + temperature + " in " + city);
+      $("#latlong").text("It's " + temperature + " in " + city + ", " + country);
+      $("#weather").text("Weather: " + weather.main);
+      console.log("http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&appid=3c764df6fcdd7a170df4a950b9f7a8f6");
+    });
   });
-
-
-});
 } else {
   /* geolocation IS NOT available */
+  $("#latlong").text("Please allow the website to use your location to work out where you are.");
 }
